97947e8e4e4817132c3a09224dcd7233b3b3f792
KRUPAL MEWADA <77066679+Krupalmewada@users.noreply.github.com>
2023-10-23 01:20:37 +0530

    Update Constants.kt

--- app/src/main/java/com/example/chatbuzzzz/constants/Constants.kt
+++ app/src/main/java/com/example/chatbuzzzz/constants/Constants.kt
@@ -3,7 +3,7 @@ package com.example.chatbuzzzz.constants
 class Constants {
     companion object{
         const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY = "AAAAHqGJaR4:APA91bFnJI_ag9HHhOAvIyKNx3OX7EnnzB_8yfa-0BhZ8i9Ed2kSSMsbXzd_gyR7RksSaIVx6hjlb48YFSLgUGwT5G_H7nHCRaF6XQFxWvApltVJuf_OazQoaogMUH6XpEAESQJGZFRV"
+        const val SERVER_KEY = "ENTER YOUR SERVER KEY"
         const val CONTENT_TYPE = "application/json"
     }
 }
